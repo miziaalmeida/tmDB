66615789b2aabb7d2dcacef712d15b3fbf6dc198
jeiellima <jeiel.lima1@gmail.com>
2024-03-28 19:29:58 -0300

    add: base view

--- TheMovie/TheMovie/Scenes/Login/LoginScreen/LoginViewController.swift
+++ TheMovie/TheMovie/Scenes/Login/LoginScreen/LoginViewController.swift
@@ -8,7 +8,41 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    private lazy var topLabel: UILabel = {
+        let label = TextLabel.createLabel(text: Text.Auth.Login.welcome)
+        return label
+    }()
+    
+    private lazy var bottomLabel: UILabel = {
+        let label = TextLabel.createLabel(text: Text.Auth.Login.toContinue)
+        return label
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+        let textField = TextField.createTf(placeholder: Text.Auth.Create.email)
+        return textField
+    }()
+    
+    private lazy var passTextField: UITextField = {
+        let textField = TextField.createTf(placeholder: Text.Auth.Create.email, isSecure: true)
+        return textField
+    }()
+    
+    private lazy var forgotButton: UIButton = {
+        let button = ActionButton.create(title: Text.Auth.Login.forgot, action: #selector(forgotPassword), target: self)
+        return button
+    }()
+    
+    private lazy var buttonLogin: UIButton = {
+        let button = ActionButton.create(title: Text.Auth.Create.login, action: #selector(goToApp), target: self)
+        return button
+    }()
+    
+    private lazy var buttonCreate: UIButton = {
+        let button = ActionButton.create(title: Text.Auth.Create.create, action: #selector(goToCreate), target: self)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
