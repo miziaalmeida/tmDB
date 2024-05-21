11c82162dfa6edbdfd2267a462a58abcd5dd2c94
jeiellima <jeiel.lima1@gmail.com>
2024-05-21 19:20:44 -0300

    add: APIManager

---
+++ TheMovie/TheMovie/Common/Services/APIManager.swift
@@ -0,0 +1,15 @@
+//
+//  APIManager.swift
+//  TheMovie
+//
+//  Created by Jeiel Lima on 21/05/24.
+//
+
+import Foundation
+
+class APIManager {
+    
+    let key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YWEyMzZhZWIzMWE3MjFkZTE2ZTQ0NTlmMTU5NTE0MSIsInN1YiI6IjY2NGQwYmNiZDI1MjFhZDVhZTEzYmVhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OjrgHXOHkNuDGPXcrTo_-HScuFmPkecOQnm_1mftwqI"
+    
+    let baseUrl = "https://api.themoviedb.org/3"
+}
