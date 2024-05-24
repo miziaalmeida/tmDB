ebf85cf871bec4d407cf55ecd1a54767222a851d
jeiellima <jeiel.lima1@gmail.com>
2024-05-24 16:36:47 -0300

    test: test sucessfull

--- TheMovie/TheMovie/Scenes/Home/View/MovieDetailView.swift
+++ TheMovie/TheMovie/Scenes/Home/View/MovieDetailView.swift
@@ -12,12 +12,14 @@ class MovieDetailView: UIView {
     lazy var titleLabel: UILabel = {
         let label = TextLabel.createLabel(text: "")
         label.font = .systemFont(ofSize: LayoutConstants.labelSize)
+        label.textColor = UIColor.black
         return label
     }()
     
     lazy var overviewLabel: UILabel = {
         let label = TextLabel.createLabel(text: "")
         label.font = .systemFont(ofSize: LayoutConstants.labelMinusSize)
+        label.textColor = UIColor.black
         label.numberOfLines = LayoutConstants.zero
         return label
     }()
@@ -39,7 +41,16 @@ class MovieDetailView: UIView {
     }
     
     private func setupConstraint() {
-        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            
+            overviewLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            overviewLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            overviewLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            overviewLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+        ])
     }
     
     func configure(with movie: Movie) {
